30271fba3d1b237b4e304acba42ed2419e5848da
Marvin Hansen <marvin.hansen@gmail.com>
2024-11-15 16:23:09 +0800

    Added test consumer
    Signed-off-by: Marvin Hansen <marvin.hansen@gmail.com>

--- queng_message_bus/message_producer/src/produce.rs
+++ queng_message_bus/message_producer/src/produce.rs
@@ -4,14 +4,10 @@ use iggy::messages::send_messages::Message;
 
 impl MessageProducer {
     pub async fn send_one(&self, message: Message) -> Result<(), IggyError> {
-        self.producer
-            .send_one(message)
-            .await
+        self.producer.send_one(message).await
     }
 
     pub async fn send_batch(&self, messages: Vec<Message>) -> Result<(), IggyError> {
-        self.producer
-            .send(messages)
-            .await
+        self.producer.send(messages).await
     }
 }
--- queng_message_bus/message_tests/src/bin/consumer/main.rs
+++ queng_message_bus/message_tests/src/bin/consumer/main.rs
@@ -1,6 +1,25 @@
 use std::error::Error;
+use iggy::clients::consumer::ReceivedMessage;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
+    let consumer_name = "test-message-consumer";
+    let config = message_shared::get_ims_data_config();
+    let message_handler =message_handler;
+
+    // build consumer
+    let mut _consumer =
+        message_consumer::MessageConsumer::from_config(&config, consumer_name, message_handler)
+            .await
+            .expect("Failed to create consumer");
+
+    // Start consumer
+    // _consumer.consume_messages().await.expect("Failed to consume messages");
+
+    Ok(())
+}
+
+fn message_handler(message: &ReceivedMessage) -> Result<(), Box<dyn Error>> {
+    println!("Received message: {:?}", &message.message);
     Ok(())
 }
